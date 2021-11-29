8d3baa946c347d2c28113537efb1321ad6163499
Thomas Marwitz <thomasmarwitz3@gmail.com>
2021-11-29 20:16:20 +0100

    initial commit

---
+++ src/main/java/edu/kit/informatik/Main.java
@@ -0,0 +1,14 @@
+package edu.kit.informatik;
+
+/**
+ * The entry point for the application
+ *
+ * @author thomas
+ * @version 1.0
+ */
+public final class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
