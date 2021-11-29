1f6c11328abb9f593b8396671133bee7bb38bca7
Thomas Marwitz <thomasmarwitz3@gmail.com>
2021-11-29 21:23:30 +0100

    modifiers

--- src/main/java/edu/kit/informatik/generator/Modifier.java
+++ src/main/java/edu/kit/informatik/generator/Modifier.java
@@ -1,4 +1,18 @@
 package edu.kit.informatik.generator;
 
 public enum Modifier {
+    METHOD("+", "==method=="),
+    STATE("#", "==state=="),
+    PROP("#", "==props=="),
+    STORE("~", null),
+
+    ;
+
+    private String sign;
+    private String delimiter;
+
+    Modifier(String sign, String delimiter) {
+        this.sign = sign;
+        this.delimiter = delimiter;
+    }
 }
