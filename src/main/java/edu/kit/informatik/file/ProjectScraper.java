da8a3956794ee02e5a1b1d2c0b473e65354e36e5
Thomas Marwitz <thomasmarwitz3@gmail.com>
2021-11-29 20:33:29 +0100

    added FileUtils with dependency

---
+++ src/main/java/edu/kit/informatik/file/ProjectScraper.java
@@ -0,0 +1,10 @@
+package edu.kit.informatik.file;
+
+import org.apache.commons.io.FileUtils;
+
+public class ProjectScraper {
+
+    public void perform() {
+        FileUtils.listFiles();
+    }
+}
